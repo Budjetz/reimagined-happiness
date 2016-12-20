6d463ea5331a0658618afd2e3eba58036cf13450
Elisha Giles <elisha.g.giles@gmail.com>
2016-12-20 13:49:49 -0700

    trying to get the side menu working

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,13 +1,25 @@
-angular.module('budjetz', ['ui.router', 'ionic']).config(function($stateProvider, $urlRouterProvider) {
+angular.module('budjetz', ['ui.router', 'ionic', 'ngMaterial']).config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
+
     .state('welcome', {
       url: '/welcome',
       templateUrl: "./public/welcome/welcomeSlides.html"
     })
     .state('home', {
       url: '/home',
-      templateUrl: "./public/home/home.html"
+      templateUrl: "./public/home/home.html",
+      controller: "homeCtrl"
     })
+        .state('home.settings', {
+          url: '/settings',
+          templateUrl: "./public/settings/settings.html"
+        })
+        .state('home.other', {
+          url: '/other',
+          templateUrl: "./public/other/other.html"
+        })
+
+    $urlRouterProvider.otherwise('/home');
 
 });
