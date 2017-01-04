08613a714d53734c0401db0d66985aec4c3f6dee
Stephen Corry <stevemcorry@gmail.com>
2017-01-04 16:52:16 -0700

    delete expenditure done

--- www/public/budget/budgetDetailsCtrl.js
+++ www/public/budget/budgetDetailsCtrl.js
@@ -5,9 +5,18 @@ angular.module('budjetz').controller('budgetDetailsCtrl', function($scope, $stat
   $scope.getSpecificExpenditure = (cat) => {
     postService.getSpecificExpenditure(cat).then((res) => {
       $scope.specificExpenditures = res.data;
-      console.log(res.data);
     })
   }
-
   $scope.getSpecificExpenditure($scope.budget);
+
+  $scope.editExpenditure = (ex) => {
+    postService.editExpenditure(ex).then((res) => {
+      console.log('edited', res);
+    })
+  }
+  $scope.deleteExpenditure = (ex) => {
+    postService.deleteExpenditure(ex).then((res) => {
+      console.log('gone', res);
+    })
+  }
 })
