697d5b398bc5790c9072cc0406c66ed80a30fd74
Elisha Giles <elisha.g.giles@gmail.com>
2017-01-04 17:08:16 -0700

    working on edit category modal

--- www/public/settings/settingsCtrl.js
+++ www/public/settings/settingsCtrl.js
@@ -15,30 +15,26 @@ angular.module('budjetz').controller('settingsCtrl', function($scope, $ionicModa
   $scope.closeModal = function(index) {
     if (index == 3) $scope.modal3.hide();
     else $scope.modal4.hide();
-  }
+  };
 
   $scope.getBudgets = () => {
     getService.getBudgets().then((res)=>{
       $scope.budgets = res.data;
-    })
-  }
+    });
+  };
   $scope.getBudgets();
 
   $scope.getSpecificExpenditure = (cat) => {
     postService.getSpecificExpenditure(cat).then((res) => {
       $scope.specificExpenditures = res.data;
-    })
-  }
-  $scope.editBudget = (bud) => {
-    postService.editBudget(bud).then( (res) => {
-      $scope.getBudgets();
-    })
-  }
+    });
+  };
+
   $scope.deleteBudget = (bud) => {
     postService.deleteBudget(bud).then( (res) => {
       $scope.getBudgets();
-    })
-  }
+    });
+  };
 
   $scope.showAdder = () => {
     $scope.bud = {};
@@ -54,17 +50,62 @@ angular.module('budjetz').controller('settingsCtrl', function($scope, $ionicModa
           text: '<div>Save</div>',
           type: 'button-positive',
           onTap: function(e){
-            $scope.count++
+            $scope.count++;
             if($scope.bud.category && $scope.bud.amount && $scope.count == 1){
               postService.addBudget($scope.bud).then( ()=>{
                 $scope.getBudgets();
                 $scope.count = 0;
-              })
+              });
             }
           }
         }
       ]
     });
-  }
+  };
 
-})
+  $scope.showConfirm = function(budget) {
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Delete ' + budget.category + "?",
+      template: 'Are you sure you want to delete the ' + budget.category + " category?"
+    });
+
+    confirmPopup.then(function(res) {
+      if(res) {
+        // console.log('You are sure');
+        $scope.deleteBudget(budget.category);
+      } else {
+        // console.log('You are not sure');
+      }
+    });
+  };
+
+//   $scope.editBudget = (bud) => {
+//     postService.editBudget(bud).then( (res) => {
+//       $scope.getBudgets();
+//     });
+//
+// };
+
+  $scope.confirmEditBudget = function(budget) {
+    var addPopup = $ionicPopup.show({
+      template:
+      '<input type="text" placeholder="' + budget.category + '" ng-model="bud.category"></input><input type="text" placeholder="' + budget.budget_amount + '" ng-model="bud.amount">',
+      title: 'Edit Budget Info',
+      scope: $scope,
+      buttons: [
+        {text: 'Cancel'},
+        {
+          text: '<div>Save</div>',
+          type: 'button-positive',
+          onTap: function(e){
+            if($scope.bud.category && $scope.bud.amount ==1) {
+              postService.editBudget(bud).then( (res) => {
+                $scope.getBudgets();
+              });
+            }
+          }
+        }
+      ]
+    });
+  };
+});
