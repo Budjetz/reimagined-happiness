c91de8a3c89c630ed9f1dcab2cc2d0fcf3bbccd8
Stephen Corry <stevemcorry@gmail.com>
2017-01-04 17:36:00 -0700

    edit expenditures finished

--- www/public/budget/budgetDetailsCtrl.js
+++ www/public/budget/budgetDetailsCtrl.js
@@ -1,22 +1,44 @@
-angular.module('budjetz').controller('budgetDetailsCtrl', function($scope, $stateParams, postService) {
+angular.module('budjetz').controller('budgetDetailsCtrl', function($scope, $stateParams, $ionicPopup, postService) {
 
   $scope.budget = $stateParams.id;
-
   $scope.getSpecificExpenditure = (cat) => {
     postService.getSpecificExpenditure(cat).then((res) => {
       $scope.specificExpenditures = res.data;
     })
   }
   $scope.getSpecificExpenditure($scope.budget);
-
   $scope.editExpenditure = (ex) => {
     postService.editExpenditure(ex).then((res) => {
       console.log('edited', res);
+      $scope.getSpecificExpenditure($scope.budget);
     })
   }
   $scope.deleteExpenditure = (ex) => {
     postService.deleteExpenditure(ex).then((res) => {
       console.log('gone', res);
+      $scope.getSpecificExpenditure($scope.budget);
     })
   }
+  $scope.showEditor = (ex) => {
+    $scope.ex = ex;
+    var addPopup = $ionicPopup.show({
+      template:
+      ' <input type="text" placeholder="location" ng-model="ex.location"> <input type="text" placeholder="amount" ng-model="ex.amount">',
+      title: 'Edit Expenditure',
+      scope: $scope,
+      buttons: [
+        {text: 'Cancel'},
+        {
+          text: '<div>Save</div>',
+          type: 'button-positive',
+          onTap: function(e){
+              $scope.editExpenditure($scope.ex);
+          }
+        }
+      ]
+    });
+  }
+
+
+
 })
