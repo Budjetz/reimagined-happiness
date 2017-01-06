e854bd72a6d640fb9095a8bd8a116a3f3158c40d
Stephen Corry <stevemcorry@gmail.com>
2017-01-06 12:54:34 -0700

    Monthly budget and totals bars finished

--- www/public/services/barChart.js
+++ www/public/services/barChart.js
@@ -22,7 +22,7 @@ angular.module('budjetz').service('barChart', function($state, getService) {
             if(d.budget_amount < d.expenditures){
               return '<div style="display: flex;"> <div style="background: #fdde2c; width: 100%; border-radius: 10px"> <div style="background: red; padding-left: 5px; border-radius: 10px; width : 100%">' + d.expenditures + '</div> </div> <div>' + d.budget_amount + '</div> </div>';
             } else {
-              return '<div style="display: flex;"> <div style="background: #1a902d; width: 100%; border-radius: 10px"> <div style="background:#fdde2c; padding-left: 5px; border-radius: 10px; width:'+ x(d.expenditures) + '%">'+d.expenditures+'</div> </div> <div>' + d.budget_amount + '</div> </div>';
+              return '<div style="display: flex;"> <div style="background: #1a902d; width: 100%; border-radius: 10px"> <div style="background:#fdde2c; padding-left: 5px; display:flex; border-radius: 10px; width:'+ x(d.expenditures) + '%">'+d.expenditures+'</div> </div> <div>' + d.budget_amount + '</div> </div>';
             }
         } else {
           return '<div style="background: gold; width: 100%; border-radius: 10px"> </div>';}});
@@ -33,8 +33,8 @@ angular.module('budjetz').service('barChart', function($state, getService) {
 
     this.makeSavingsBar = () => {
       getService.getBudgetExpenditures().then( (res) => {
-        if(d3.select('.totals')[0]){
-          d3.selectAll('.totals').remove();
+        if(d3.select('.savings')[0]){
+          d3.selectAll('.savings').remove();
         }
         var data = {}
             final = []
@@ -51,12 +51,12 @@ angular.module('budjetz').service('barChart', function($state, getService) {
         final.push(data)
         return final
       }).then( (data) => {
-          var main = d3.select('.totalBar')
-            .selectAll('.totalBar')
+          var main = d3.select('.savingsBar')
+            .selectAll('.savingsBar')
             .data(data)
             .enter()
             .append('div')
-            .attr('class', 'totals')
+            .attr('class', 'savings')
             .style('width','auto')
             .style('margin','10px')
             .html(function (d) {
@@ -69,9 +69,36 @@ angular.module('budjetz').service('barChart', function($state, getService) {
                   return '<div style="display: flex;"> <div style="background: #1a902d; width: 100%; border-radius: 10px"> <div style="background:red; padding-left: 5px; border-radius: 10px; width:100%">'+d.two+'</div> </div> <div>' + d.one + '</div> </div>'
                 }
             })
+            makeTotalBar(data)
         })
 
+        makeTotalBar = (data) =>{
 
-
-    }
+          if(d3.select('.totals')[0]){
+            d3.selectAll('.totals').remove();
+          }
+          getService.getMoneyTotal().then( (res) => {
+            data[0].total = res.data[0].amount;
+            console.log(res.data[0].amount);
+            var main = d3.select('.totalBar')
+              .selectAll('.totalBar')
+              .data(data)
+              .enter()
+              .append('div')
+              .attr('class', 'totals')
+              .style('width','auto')
+              .style('margin','10px')
+              .html(function (d) {
+                var x = d3.scale.linear()
+                  .domain([0, d.total])
+                  .range([0, 100]);
+                  if(d.total > d.two){
+                    return '<div style="display: flex;"> <div style="background: #1a902d; width: 100%; border-radius: 10px"> <div style="background:#fdde2c; padding-left: 5px; border-radius: 10px; width:'+ x(d.two) + '%">'+d.two+'</div> </div> <div>' + d.total + '</div> </div>'
+                  } else {
+                    return '<div style="display: flex;"> <div style="background: #1a902d; width: 100%; border-radius: 10px"> <div style="background:red; padding-left: 5px; border-radius: 10px; width:100%">'+d.two+'</div> </div> <div>' + d.total + '</div> </div>'
+                  }
+              })
+            })
+        }
+      }
 });
