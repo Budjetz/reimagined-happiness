424afefa475fbd83b2039985f7b8f03438c904b4
Stephen Corry <stevemcorry@gmail.com>
2017-01-12 17:23:31 -0700

    comment out passport

--- www/index.js
+++ www/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const passport = require('passport');
 const massive = require('massive');
 const moment = require('moment');
-const FacebookStrategy = require('passport-facebook').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
+// const passport = require('passport');
 // const config = require('./config.js');
 
 const app = module.exports = express();
@@ -19,8 +19,8 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.static('./public'));
 app.use('/ocr', express.static(__dirname + '/ocrWork/www'));
 //database
@@ -30,67 +30,67 @@ const db = app.get('db');
 const serverController = require('./serverController.js');
 
 //Facebook Authorization
-passport.use(new FacebookStrategy({
-    clientID: process.env.facebookID,
-    clientSecret: process.env.facebookSecret,
-    callbackURL: "/auth/facebook/callback"
-  }, (accessToken, refreshToken, profile, next) => {
-   console.log('FB Profile: ', profile);
-    //db. query to check if user exists in database
-    db.users.findOne({fb_id: profile.id}, (err, dbRes) => {
-      if (!dbRes) {
-        console.log("User not found. Creating...");
-        db.users.insert({fb_id: profile.id, name: profile.displayName, pic: profile.picture}, (err, dbRes) => {
-          console.log(dbRes);
-          req.user.id = dbRes.id;
-          console.log(req.user);
-          return next(null, dbRes);
-        });
-      } else {
-        console.log('here',dbRes);
-        return next(null, dbRes);
-      }
-    });
-  }
-));
-
-passport.serializeUser((profile, done) => {
-  console.log('profile',profile);
-  done(null, profile);
-});
-
-passport.deserializeUser((deserializedUser, done) => {
-  //console.log('des');
-  done(null, deserializedUser);
-});
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-  successRedirect: '/#/welcome',
-  failureRedirect: '/#/'
-}));
-
-const checkAuth = (req, res, next) => {
-  // console.log("checkAuth");
-  console.log(req.user);
-  if (req.isAuthenticated()) {
-    next();
-  }
-  else {
-    res.json({err: 403});
-  }
-};
-
-app.get('/me', checkAuth, (req, res, next) => {
-  console.log("req.user in server /me", req.user);
-   res.json(req.user);
-});
-
-app.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
+// passport.use(new FacebookStrategy({
+//     clientID: process.env.facebookID,
+//     clientSecret: process.env.facebookSecret,
+//     callbackURL: "/auth/facebook/callback"
+//   }, (accessToken, refreshToken, profile, next) => {
+//    console.log('FB Profile: ', profile);
+//     //db. query to check if user exists in database
+//     db.users.findOne({fb_id: profile.id}, (err, dbRes) => {
+//       if (!dbRes) {
+//         console.log("User not found. Creating...");
+//         db.users.insert({fb_id: profile.id, name: profile.displayName, pic: profile.picture}, (err, dbRes) => {
+//           console.log(dbRes);
+//           req.user.id = dbRes.id;
+//           console.log(req.user);
+//           return next(null, dbRes);
+//         });
+//       } else {
+//         console.log('here',dbRes);
+//         return next(null, dbRes);
+//       }
+//     });
+//   }
+// ));
+//
+// passport.serializeUser((profile, done) => {
+//   console.log('profile',profile);
+//   done(null, profile);
+// });
+//
+// passport.deserializeUser((deserializedUser, done) => {
+//   //console.log('des');
+//   done(null, deserializedUser);
+// });
+//
+// app.get('/auth/facebook', passport.authenticate('facebook'));
+//
+// app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+//   successRedirect: '/#/welcome',
+//   failureRedirect: '/#/'
+// }));
+//
+// const checkAuth = (req, res, next) => {
+//   // console.log("checkAuth");
+//   console.log(req.user);
+//   if (req.isAuthenticated()) {
+//     next();
+//   }
+//   else {
+//     res.json({err: 403});
+//   }
+// };
+//
+// app.get('/me', checkAuth, (req, res, next) => {
+//   console.log("req.user in server /me", req.user);
+//    res.json(req.user);
+// });
+//
+// app.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
 
 //Other
 app.get('/getImage', serverController.getImage);
